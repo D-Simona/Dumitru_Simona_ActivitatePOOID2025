64420dcb3774b4ad1bf090d72ec1679112dd5377
Simona Dumitru <simonaionela.d@gmail.com>
2025-11-10 17:23:58 +0200

    ex2

---
+++ Ex2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Activitate {
+	const int idActivitate;
+	char* denumire;
+	string locatie;
+protected:
+
+public:
+	static int numarator;
+
+};
+
+
+void main() {
+
+}
