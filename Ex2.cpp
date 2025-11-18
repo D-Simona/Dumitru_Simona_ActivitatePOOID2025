7f1067a165d6f1be9fba977322fa978895f197aa
Simona Dumitru <simonaionela.d@gmail.com>
2025-11-18 15:04:27 +0200

    ex2

--- Ex2.cpp
+++ Ex2.cpp
@@ -179,6 +179,95 @@ public:
 	{
 		return this->gradDificultate;
 	}
+
+	//operatorul =
+	Activitate& operator=(const Activitate& a)
+	{
+		if (this != &a)
+		{
+			if (this->denumire != NULL)
+			{
+				delete[]this->denumire;
+			}
+			if (this->denumireObiecte != NULL)
+			{
+				delete[]this->denumireObiecte;
+			}
+			if (this->greutateObiecte != NULL)
+			{
+				delete[]this->greutateObiecte;
+			}
+			if (strlen(denumire) >= 3)
+			{
+				this->denumire = new char[strlen(a.denumire) + 1];
+				strcpy(this->denumire, a.denumire);
+			}
+			else
+			{
+				this->denumire = new char[strlen("Necunoscut") + 1];
+				strcpy(this->denumire, "Necunoscut");
+			}
+			if (locatie.size() >= 3)
+			{
+				this->locatie = a.locatie;
+			}
+			else
+			{
+				this->locatie = "Necunoscuta";
+			}
+			if (durata > 0)
+			{
+				this->durata = a.durata;
+			}
+			else
+			{
+				this->durata = 0;
+			}
+
+			if (nrObiecte > 0 && denumireObiecte != NULL & greutateObiecte != NULL)
+			{
+				this->nrObiecte = a.nrObiecte;
+				this->denumireObiecte = new string[this->nrObiecte];
+				for (int i = 0; i < this->nrObiecte; i++)
+				{
+					this->denumireObiecte[i] = a.denumireObiecte[i];
+				}
+				this->greutateObiecte = new float[this->nrObiecte];
+				for (int i = 0; i < this->nrObiecte; i++)
+				{
+					this->greutateObiecte[i] = a.greutateObiecte[i];
+				}
+			}
+			else
+			{
+				this->nrObiecte = 0;
+				this->denumireObiecte = NULL;
+				this->greutateObiecte = NULL;
+			}
+			this->gradDificultate = a.gradDificultate;
+			
+			return *this;
+		}
+		
+	}
+
+	//destructor
+	~Activitate()
+	{
+		if (this->denumire != NULL)
+		{
+			delete[]this->denumire;
+		}
+		if (this->denumireObiecte != NULL)
+		{
+			delete[]this->denumireObiecte;
+		}
+		if (this->greutateObiecte != NULL)
+		{
+			delete[]this->greutateObiecte;
+		}
+	}
+
 };
 
 //initializarea staticului
@@ -190,4 +279,24 @@ void main() {
 	string denumiri[] = { "bara", "haltere" };
 	float greutate[] = { 20, 17.5 };
 	Activitate a1("Sala", "Acasa", 30, 2, denumiri, greutate, Usoara);
+
+	cout << a1.getIdActivitate() << endl;
+	cout << a1.getDenumire() << endl;
+	cout << a1.getLocatie() << endl;
+	cout << a1.getDurata() << endl;
+	cout << a1.getNrObiecte() << endl;
+	for (int i = 0; i < a1.getNrObiecte(); i++)
+	{
+		cout << a1.getDenumireObiecte()[i] << endl;
+
+	}
+	for (int i = 0; i < a1.getNrObiecte(); i++)
+	{
+		cout << a1.getGreutateObiecte()[i] << endl;
+	}	
+	cout << a1.getGradDificultate() << endl;
+
+	Activitate a2 = a1;
+
+	
 }
