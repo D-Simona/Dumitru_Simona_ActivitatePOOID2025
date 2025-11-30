22c026e37cda489b69d36447cec8132f6d09dc07
Simona Dumitru <simonaionela.d@gmail.com>
2025-11-30 16:01:27 +0200

    ex 5

---
+++ Ex5.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Calatorie 
+{
+private:
+	char* numeDestinatie;
+	const int idCalatorie;
+public:
+
+	Calatorie(const char* numeDestinatie) :idCalatorie(1)
+	{
+		if (strlen(numeDestinatie) >=3)
+		{
+			this->numeDestinatie = new char[strlen(numeDestinatie) + 1];
+			strcpy_s(this->numeDestinatie, strlen(numeDestinatie) + 1, numeDestinatie);
+		}
+		else
+		{
+			this->numeDestinatie = new char[strlen("N/A")+1];
+			strcpy_s(this->numeDestinatie, strlen("N/A") + 1, "N/A");
+		}
+	}
+
+	int getIdCalatorie()
+	{
+		return this->idCalatorie;
+	}
+
+	void setNumeDestinatie(char* numeDestinatie)
+	{
+		if (strlen(numeDestinatie) >= 3)
+		{
+			this->numeDestinatie = new char[strlen(numeDestinatie) + 1];
+			strcpy_s(this->numeDestinatie, strlen(numeDestinatie) + 1, numeDestinatie);
+		}
+		else
+		{
+			this->numeDestinatie = new char[strlen("N/A") + 1];
+			strcpy_s(this->numeDestinatie, strlen("N/A") + 1, "N/A");
+		}
+	}
+
+	Calatorie operator=(const Calatorie& c)
+	{
+		if (this != &c)
+		{
+			if (this->numeDestinatie!=NULL)
+			{
+				delete[]this->numeDestinatie;
+				this->numeDestinatie = NULL;
+			}
+			if(c.numeDestinatie!=NULL)
+			{
+				this->numeDestinatie = new char[strlen(c.numeDestinatie) + 1];
+				strcpy_s(this->numeDestinatie, strlen(c.numeDestinatie) + 1, c.numeDestinatie);
+			}
+			else
+			{
+				this->numeDestinatie = NULL;
+			}
+		}
+		return *this;
+	}
+
+	friend void operator<<(ostream& out, Calatorie c)
+	{
+		out << "Id: " << c.idCalatorie;
+		out<< (c.numeDestinatie != NULL ? "Nume destinatie: " + string(c.numeDestinatie) : "Nume nespecificat");
+	}
+
+	void operator()(char* numeDestinatie)
+	{
+
+		if (strlen(numeDestinatie) >= 3)
+		{
+			this->numeDestinatie = new char[strlen(numeDestinatie) + 1];
+			strcpy_s(this->numeDestinatie, strlen(numeDestinatie) + 1, numeDestinatie);
+		}
+		else
+		{
+			this->numeDestinatie = new char[strlen("N/A") + 1];
+			strcpy_s(this->numeDestinatie, strlen("N/A") + 1, "N/A");
+		}
+	}
+};
+
+void main()
+{
+	Calatorie c1("Brasov");
+	cout<<c1.getIdCalatorie()<<endl;
+	c1.setNumeDestinatie("Craiova");
+	Calatorie c2("Bacau");
+	c2 = c1;
+	cout << c2;
+	c2("Constanta");
+
+}
